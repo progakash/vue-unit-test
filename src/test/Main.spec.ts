83f40f6676af617f56cd7582512697d10417f2a0
akash.ju.msc@gmail.com <akash.ju.msc@gmail.com>
2023-11-21 23:28:20 +0600

    should render msg property

--- src/test/Main.spec.ts
+++ src/test/Main.spec.ts
@@ -1,6 +1,15 @@
 import { mount } from "@vue/test-utils"
-import { it } from "vitest"
+import { expect, it } from "vitest"
+import Main from '../components/Main.vue'
 
-it("test case test", () => {
-    console.log("hello akash")
+it("should render msg property", () => {
+    const instance = mount(Main, {
+        props: {
+            msg: 'Hello Aaira'
+        }
+    });
+
+    
+    expect(instance.find('h1').text()).toBe('Hello Aaira')
+    //console.log(instance.find('h1').text())
 })
