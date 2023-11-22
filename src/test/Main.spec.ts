1203c694b1b4b3b457a66a89f83547da19ee048c
akash.ju.msc@gmail.com <akash.ju.msc@gmail.com>
2023-11-22 22:27:45 +0600

    white box testing

--- src/test/Main.spec.ts
+++ src/test/Main.spec.ts
@@ -2,7 +2,7 @@ import { mount } from "@vue/test-utils"
 import { expect, it } from "vitest"
 import Main from '../components/Main.vue'
 
-it("should render msg property", () => {
+it('should render msg property', () => {
     const instance = mount(Main, {
         props: {
             msg: 'Hello Aaira'
@@ -12,4 +12,19 @@ it("should render msg property", () => {
     
     expect(instance.find('h1').text()).toBe('Hello Aaira')
     //console.log(instance.find('h1').text())
+})
+
+//white box testing
+it('should increment count when the increment method is called', () => {
+    const instance = mount(Main);
+
+    /**
+     * here, call method from component
+     * so, when call increment method it will be count increment; 0-->1
+     * This is the Vue instance.
+     * You can access all the instance methods and properties of a vm with wrapper.vm. 
+     */
+    instance.vm.increment();
+
+    expect(instance.vm.count).toBe(1)
 })
